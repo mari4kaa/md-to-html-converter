c70eba845697cb7dbf44715b0ffae2c27b69d28f
Mariia Kryvokhata <m.krivokhata@gmail.com>
2024-02-29 23:18:20 +0200

    Move regular expressions to the collection

--- Converter.js
+++ Converter.js
@@ -5,6 +5,11 @@ class Converter {
     this.html = '';
     this.inPreformattedText = false;
     this.inParagraph = false;
+    this.regexps = {
+      bold: /\*\*(.*?)\*\*/g,
+      italic: /(?<!\w)(_)(\w+(?:\s+\w+)*)(_)(?!\w)/g,
+      monospaced: /`(.*?)`/g
+    };
   }
 
   convertMd = (markdown) => {
@@ -30,9 +35,9 @@ class Converter {
         }
 
         let currentLine = line;
-        currentLine = currentLine.replace(/\*\*(.*?)\*\*/g, '<b>$1</b>');
-        currentLine = currentLine.replace(/(?<!\w)(_)(\w+(?:\s+\w+)*)(_)(?!\w)/g, '<i>$2</i>');
-        currentLine = currentLine.replace(/`(.*?)`/g, '<tt>$1</tt>');
+        currentLine = currentLine.replace(this.regexps.bold, '<b>$1</b>');
+        currentLine = currentLine.replace(this.regexps.italic, '<i>$2</i>');
+        currentLine = currentLine.replace(this.regexps.monospaced, '<tt>$1</tt>');
 
         this.html += currentLine + '\n';
       }
