a09cf585cd9a21c5a4f101d88f965d493150e551
Mariia Kryvokhata <m.krivokhata@gmail.com>
2024-02-29 18:13:23 +0200

    Add basic converting function

--- app.js
+++ app.js
@@ -3,6 +3,46 @@
 const { program } = require('commander');
 const fs = require('fs');
 
+const convertMd = (markdown) => {
+  let html = '';
+  let inPreformattedText = false;
+  let inParagraph = false;
+
+  const lines = markdown.split('\n');
+
+  for (const line of lines) {
+    if (line.trim() === '```') {
+      inPreformattedText = !inPreformattedText;
+      html += inPreformattedText ? '<pre>\n' : '</pre>\n';
+    } else if (inPreformattedText) {
+      html += `${line}\n`;
+    } else if (line.trim() === '') {
+      if (inParagraph) {
+        html += '</p>\n';
+        inParagraph = false;
+      }
+    } else {
+      if (!inParagraph) {
+        html += '<p>';
+        inParagraph = true;
+      }
+
+      let processedLine = line;
+      processedLine = processedLine.replace(/\*\*(.*?)\*\*/g, '<b>$1</b>');
+      processedLine = processedLine.replace(/(?<!\w)(_)(\w+(?:\s+\w+)*)(_)(?!\w)/g, '<i>$2</i>');
+      processedLine = processedLine.replace(/`(.*?)`/g, '<tt>$1</tt>');
+
+      html += processedLine + '\n';
+    }
+  }
+
+  if (inParagraph) {
+    html += '</p>\n';
+  }
+
+  return html;
+};
+
 program
   .version('1.0.0')
   .arguments('<filePath>')
@@ -24,7 +64,3 @@ program
   });
 
 program.parse(process.argv);
-
-const convertMd = (data) => {
-
-}
