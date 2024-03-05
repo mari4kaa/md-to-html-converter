9a0089e675a04f9bb1341b02681aab950698d4d2
Mariia Kryvokhata <m.krivokhata@gmail.com>
2024-03-05 21:56:28 +0200

    Decompose converter

--- Converter.js
+++ Converter.js
@@ -9,38 +9,50 @@ class Converter {
     this.inParagraph = false;
   }
 
-  convertMd = (markdown) => {
+  convertMd (markdown) {
     this.markdown = markdown;
     const lines = markdown.split('\n');
 
     for (const line of lines) {
       if (line.startsWith('// ')) continue;
 
-      if (line.trim() === tags.preformatted.md) {
-        this.html += this.inPreformattedText ? `${tags.preformatted.close}\n` : `${tags.preformatted.open}\n`;
-        this.inPreformattedText = !this.inPreformattedText;
-      } else if (this.inPreformattedText) {
-        this.html += `${line}\n`;
-      } else if (line.trim() === '') {
-        if (this.inParagraph) {
-          this.html += '</p>\n';
-          this.inParagraph = false;
-        }
-      } else {
-        if (!this.inParagraph) {
-          this.html += '<p>\n';
-          this.inParagraph = true;
-        }
-
-        const htmlLine = this.replaceFormattingTags(line) + '\n';
-        this.html += htmlLine;
-      }
+      this.handlePreformattedText(line);
+      this.handleEmptyLine(line);
+      this.handleParagraph(line);
     }
 
     if (this.inParagraph) this.html += '</p>\n';
 
     return this.html;
-  };
+  }
+
+  handlePreformattedText (line) {
+    if (line.trim() === tags.preformatted.md) {
+      this.html += this.inPreformattedText ? `${tags.preformatted.close}\n` : `${tags.preformatted.open}\n`;
+      this.inPreformattedText = !this.inPreformattedText;
+    } else if (this.inPreformattedText) {
+      this.html += `${line}\n`;
+    }
+  }
+
+  handleEmptyLine (line) {
+    if (line.trim() === '' && this.inParagraph) {
+      this.html += '</p>\n';
+      this.inParagraph = false;
+    }
+  }
+
+  handleParagraph (line) {
+    if (line.trim() !== '' && !this.inParagraph) {
+      this.html += '<p>\n';
+      this.inParagraph = true;
+    }
+
+    if (line.trim() !== '') {
+      const htmlLine = this.replaceFormattingTags(line) + '\n';
+      this.html += htmlLine;
+    }
+  }
 
   replaceFormattingTags (line) {
     let currentLine = line;
@@ -49,7 +61,7 @@ class Converter {
     currentLine = currentLine.replace(regexps.monospaced, `${tags.monospaced.open}$1${tags.monospaced.close}`);
 
     return currentLine;
-  };
-};
+  }
+}
 
 module.exports = { Converter };
